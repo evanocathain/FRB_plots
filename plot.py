95e09e286688c736022403dc0d544f0620077862
Evan Keane <e.keane@ekeane-lt.skatelescope.org>
2017-04-10 11:45:07 +0100

    Add PKS alt-zen plot
    Now also plots an alt-zen plot for the Parkes FRBs. Also some tidying.

--- plot.py
+++ plot.py
@@ -22,28 +22,30 @@ if args.update:
 
 # Read the CSV file from FRBCAT
 import numpy as np
-frbs = np.zeros((56,3), dtype=object)
+frbs = np.zeros((100,5), dtype=object)
 i = 0
 import csv, sys
 file = open("frbcat.csv", 'rb')
 try:
     reader = csv.DictReader(file)
     for row in reader: 
-        array = row['Name'],row['RAJ'], row['DECJ']           
+        array = row['Name'],row['RAJ'],row['DECJ'],row['UTC'],row['Telescope']
         frbs[i]=array
         i = i +1
 finally:
     file.close()  
+gl = np.zeros(i)
+gb = np.zeros(i)
+ids = frbs[:,0]
+utc = frbs[:,3]
+tel = frbs[:,4]
 
+# Convert RA & DEC to gl & gb
 import ephem as e
 import math as m
 deg2rad=m.pi/180.0
 rad2deg=1.0/deg2rad
-ids = np.zeros(56,dtype=object) # FRB names
-gl = np.zeros(56)
-gb = np.zeros(56)
 for i in range(0,56):
-    ids[i] = frbs[i][0]
     radec = e.Equatorial(frbs[i][1],frbs[i][2], epoch='2000')
     g = e.Galactic(radec)
     if (g.lon > m.pi):
@@ -55,15 +57,63 @@ for i in range(0,56):
     #print('%s %s' % (g.lon,g.lat))
 #    print('%s %s' % (g.lon*rad2deg,g.lat*rad2deg))
 
-print gl*rad2deg,gb*rad2deg
+az = np.zeros(56)
+alt = np.zeros(56)
+# Convert RA, DEC & UTC to alt & az
+for i in range(0,56):
+    frb = e.FixedBody()
+    frb._ra = frbs[i][1]
+    frb._dec = frbs[i][2]
+    site=e.Observer()
+    if (tel[i]=="parkes"):
+        site.long = '148:15:44.3'
+        site.lat = '-32:59:59.8'
+        site.elevation = 0
+        place = "Parkes Observatory"
+    elif (tel[i]=="GBT"):
+        site.long = '-79:50:23'
+        site.lat = '38:25:59'
+        site.elevation = 0
+        place = "Green Bank Telescope"
+    elif (tel[i]=="arecibo"):
+        site.long = '-66:45:10'
+        site.lat = '18:20:39'
+        site.elevation = 0
+        place = "Arecibo"
+    elif (tel[i]=="UTMOST"):
+        site.long = '149.4241'
+        site.lat = '35.3707'
+        site.elevation = 0
+        place = "Molonglo"
+    site.date = utc[i]
+    frb.compute(site)
+    if (place == "Parkes Observatory"):
+        az[i]=frb.az
+        alt[i]=frb.alt
+#        print ids[i],frb.az,frb.alt
+
+#print gl*rad2deg,gb*rad2deg
+# Plot things
+# Aitoff projection
 import matplotlib.pyplot as plt
 plt.figure()
 plt.subplot(111, projection="aitoff")
-#plt.title("FRB Galactic Coordinate Distribution - Aitoff Projection")
+plt.title("FRB Galactic Coordinate Distribution - Aitoff Projection")
 plt.grid(True)
 plt.plot(gl,gb, 'o')
 # Optional plotting features
 if args.id:
     for i in range(0,56):
         plt.text(gl[i],gb[i],ids[i],fontsize=10) # Obviously this can be tweaked
+plt.show()
+
+print az*rad2deg,alt*rad2deg
+# Alt-az plot
+plt.clf()
+sp = plt.subplot(111, projection="polar")
+plt.title("Parkes FRB Zen-Az Distribution")
+plt.grid(True)
+#plt.plot(az*rad2deg,90.0-alt*rad2deg, 'o')
+sp.set_theta_zero_location("N")
+plt.plot(az,(0.5*m.pi-alt)*rad2deg, 'o')
 plt.show()
